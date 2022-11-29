3d014626cb5d4a2fb610df2ca39f8ac0549e14d9
frantrassierra <94199239+frantrassierra@users.noreply.github.com>
2022-11-29 13:27:40 +0100

    323
    Crea la clase Juego la cual hereda de Soporte. Añade los atributos consola, minNumJugadores y maxNumJugadores. A continuación añade el método muestraJugadoresPosibles, el cual debe mostrar Para un jugador, Para X jugadores o De X a Y jugadores dependiendo de los valores de las atributos creados. Finalmente, sobreescribe tanto el contructor como el método muestraResumen.
    
    Añade a inicio.php el código para probar la clase:

--- inicio.php
+++ inicio.php
@@ -1,13 +1,12 @@
 <?php
 include "Soporte.php";
-
 include "CintaVideo.php";
-
 include "Dvd.php";
+include "Juego.php";
 
-$miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
-echo "<strong>" . $miDvd->titulo . "</strong>";
-echo "<br>Precio: " . $miDvd->getPrecio() . " euros";
-echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
-$miDvd->muestraResumen();
+$miJuego = new Juego("The Last of Us Part II", 26, 49.99, "PS4", 1, 1);
+echo "<strong>" . $miJuego->titulo . "</strong>";
+echo "<br>Precio: " . $miJuego->getPrecio() . " euros";
+echo "<br>Precio IVA incluido: " . $miJuego->getPrecioConIva() . " euros";
+$miJuego->muestraResumen();
 ?>
