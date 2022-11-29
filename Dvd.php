b1ecf43caf4abb78db5e8801c75c7ae42bd6ddae
frantrassierra <94199239+frantrassierra@users.noreply.github.com>
2022-11-29 13:26:27 +0100

    322
    Crea la clase Dvd la cual hereda de Soporte. Añade los atributos idiomas y formatoPantalla. A continuación sobreescribe tanto el contructor como el método muestraResumen.
    
    Añade a inicio.php el código para probar la clase:

---
+++ Dvd.php
@@ -0,0 +1,18 @@
+<?php
+class Dvd extends Soporte {
+    public $idiomas;
+    private $formatoPantalla;
+
+    public function __construct($titulo, $numero, $precio,$idiomas,$formatoPantalla)
+    {
+        parent::__construct($titulo, $numero, $precio);
+        $this->idiomas=$idiomas;
+        $this->formatoPantalla=$formatoPantalla;
+    }
+    public function muestraResumen()
+    {
+        echo "<br> Película en DVD ";
+        echo  parent::muestraResumen() . "<br>Idiomas: " .$this->idiomas. " <br>Formato Pantalla: ".$this->formatoPantalla;
+    }
+}
+?>
--- inicio.php
+++ inicio.php
@@ -3,10 +3,11 @@ include "Soporte.php";
 
 include "CintaVideo.php";
 
+include "Dvd.php";
 
-$miCinta = new CintaVideo("Los cazafantasmas", 23, 3.5, 107);
-echo "<strong>" . $miCinta->titulo . "</strong>";
-echo "<br>Precio: " . $miCinta->getPrecio() . " euros";
-echo "<br>Precio IVA incluido: " . $miCinta->getPrecioConIva() . " euros";
-$miCinta->muestraResumen();
+$miDvd = new Dvd("Origen", 24, 15, "es,en,fr", "16:9");
+echo "<strong>" . $miDvd->titulo . "</strong>";
+echo "<br>Precio: " . $miDvd->getPrecio() . " euros";
+echo "<br>Precio IVA incluido: " . $miDvd->getPrecioConIva() . " euros";
+$miDvd->muestraResumen();
 ?>
